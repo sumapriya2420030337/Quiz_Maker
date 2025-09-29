055958e930a154f43b1198646a390293ca6ca030
suma priya <2420030337@klh.edu.in>
2025-09-29 21:22:49 +0530

    Delete QLD directory

--- QLD/result.js
+++
@@ -1,7 +0,0 @@
-const score = localStorage.getItem("quizScore") || 0;
-document.getElementById("score").textContent = `Your Score: ${score}`;
-
-let feedback = "Keep trying!";
-if (score == 2) feedback = "Excellent!";
-else if (score == 1) feedback = "Good job!";
-document.getElementById("feedback").textContent = feedback;
